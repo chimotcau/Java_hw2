5ccee34f20fc945047df05797974b8988fbf5ef1
chimotcau <khoacenzo@gmail.com>
2025-12-05 21:05:00 +0300

    Remove files

--- Fish.java
+++
@@ -1,5 +0,0 @@
-class Fish extends Animal {
-    public Fish(int id, double height){
-        super(id, height);
-    }
-}
--- Hippo.java
+++
@@ -1,9 +0,0 @@
-class Hippo extends Animal implements Sound {
-    public Hippo(int id, double height) {
-        super(id, height);
-    }
-    @Override
-    public void makesound() {
-        System.out.println("Gu Gu");
-    }
-}
